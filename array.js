311cbdc835b5b47477af480156caa0fb3a641a8c
alekinho081 <115739916+alekinho081@users.noreply.github.com>
2025-02-11 17:36:34 -0300

    Add files via upload

---
+++ array.js
@@ -0,0 +1,20 @@
+let palavras = []
+
+    for(i = 0; i < palavras.lenght; i++){
+        palavras.push(pala)
+    }
+
+function addElemento(){
+    let palavra = document.getElementById("pala").value 
+    palavras.push(palavra)
+}
+
+function mostraArray(){
+    alert(palavras + '\n' + "O total de caracteres é " + calculaTotal())
+}
+
+function calculaTotal(){
+    let total = ''
+    palavras.forEach((items) => total += items)
+    return(total.length)
+}
---
+++ verificaCPF.js
@@ -0,0 +1,46 @@
+function verificaCPF(CPF){
+    //Verifica comprimento do CPF
+    if(CPF.lenght = 11){
+        //Declarando variaveis e dividindo CPF
+        let cpf = CPF.toString().split("").map(Number)
+        let digitoVerifica = ""
+        let num1, total1 = 0, digVer1
+        let num2, total2 = 0, digVer2
+
+        //Multiplicando e somando digitos
+        for(i = 0; i < 9; i++){
+            num1 = parseInt(cpf[i]) * (10 - i)
+            total1 += num1
+        }
+        if(total1 % 11 < 2){
+            digVer1 = 0
+        }else{
+            digVer1 = 11 - (total1 % 11)
+        }
+
+        for(i = 0; i < 9; i++){
+            num2 = parseInt(cpf[i]) * (11 - i)
+            total2 += num2
+        }
+        if(total2 % 11 < 2){
+            digVer2 = 0
+        }else{
+            digVer2 = 11 - (total2 % 11)
+        }
+        digitoVerifica = digVer1.toString + digVer2.toString
+        parseInt(digitoVerifica)
+        let cpfes = cpf[9].toString + cpf[10].toString
+        parseInt(cpfes)
+        if(digitoVerifica === cpfes){
+            console.log("CPF valido")
+        }else {
+            console.log("CPF invalido")
+        }
+    }
+    
+}
+
+verificaCPF(12345678910)
+verificaCPF(11111111111)
+verificaCPF(10582122902)
+
